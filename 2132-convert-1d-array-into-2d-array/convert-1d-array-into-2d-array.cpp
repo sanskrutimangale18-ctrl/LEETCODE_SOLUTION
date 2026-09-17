c8ec4804a127e3888b41a549fa0c122f3cb48e90
sanskrutimangale18-ctrl <sanskrutimangale18@gmail.com>
2026-09-17 10:19:51 +0530

    Time: 3 ms (99.41%) | Memory: 89.8 MB (80.49%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         int a=original.size();
-        vector<vector<int>>mat;
+        
         if(a!= m*n){
-            return mat;
+            return {};
         }
 
-        int rows=m, cols=n;
-        mat.resize(rows);
-
-        for(int i=0;i <rows; i++){
-            mat[i].resize(cols,0);
-        }
+        vector<vector<int>>mat(m, vector<int>(n));
 
         for(int i=0;i< m*n; i++){
             mat[i/n][i%n]=original[i];
